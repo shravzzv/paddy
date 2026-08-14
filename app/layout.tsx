ffa3aa293b0c83daa4cd2504d3cd40a2c44315d7
Sai Shravan <saishravan384@gmail.com>
2026-08-14 20:58:12 +0530

    feat: setup dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { cn } from '@/lib/utils'
+import { ThemeProvider } from '@/providers/theme-provider'
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-sans' })
 
@@ -13,8 +14,21 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: LayoutProps<'/'>) {
   return (
-    <html lang='en' className={cn('antialiased', 'font-sans', inter.variable)}>
-      <body>{children}</body>
+    <html
+      lang='en'
+      suppressHydrationWarning
+      className={cn('antialiased', 'font-sans', inter.variable)}
+    >
+      <body>
+        <ThemeProvider
+          attribute='class'
+          defaultTheme='system'
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { ThemeToggle } from '@/components/theme-toggle'
+
 export default function Page() {
   return (
     <div>
       <p>Welcome to Paddy.</p>
+      <ThemeToggle />
     </div>
   )
 }
