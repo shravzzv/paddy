95350df3bfc9a6170a620c3d73ceac4e313ab378
Sai Shravan <saishravan384@gmail.com>
2026-08-19 08:51:37 +0530

    feat: render a PDF's page in a basic reading surface

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,71 @@
-import { ThemeToggle } from '@/components/theme-toggle'
+'use client'
+
+import { Field, FieldDescription, FieldLabel } from '@/components/ui/field'
+import { Input } from '@/components/ui/input'
+import { useEffect, useRef, useState } from 'react'
 
 export default function Page() {
+  const [file, setFile] = useState<File | null>(null)
+  const canvasRef = useRef<HTMLCanvasElement>(null)
+
+  useEffect(() => {
+    if (!file) return
+
+    const loadPdf = async () => {
+      const pdfjs = await import('pdfjs-dist')
+
+      pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+        'pdfjs-dist/build/pdf.worker.mjs',
+        import.meta.url,
+      ).toString()
+
+      const arrayBuffer = await file.arrayBuffer()
+      const loadingPdf = pdfjs.getDocument({ data: arrayBuffer })
+      const pdf = await loadingPdf.promise
+
+      const page = await pdf.getPage(1)
+      const viewport = page.getViewport({ scale: 1 })
+
+      const canvas = canvasRef.current
+      if (!canvas) return
+      const context = canvas.getContext('2d')
+      if (!context) return
+
+      const renderContext = {
+        canvasContext: context,
+        viewport,
+        canvas,
+      }
+
+      await page.render(renderContext).promise
+    }
+
+    loadPdf()
+  }, [file])
+
   return (
-    <div>
-      <p>Welcome to Paddy.</p>
-      <ThemeToggle />
+    <div className='mx-auto w-full max-w-xl py-6'>
+      <Field>
+        <FieldLabel htmlFor='pdf'>File</FieldLabel>
+
+        <Input
+          id='pdf'
+          type='file'
+          accept='.pdf'
+          onChange={(e) => setFile(e.target.files?.[0] ?? null)}
+        />
+
+        <FieldDescription>Please select a PDF file to upload.</FieldDescription>
+      </Field>
+
+      {file && (
+        <canvas
+          ref={canvasRef}
+          width={576}
+          height={790}
+          className='my-8 overflow-auto rounded-xl border shadow-lg'
+        ></canvas>
+      )}
     </div>
   )
 }
